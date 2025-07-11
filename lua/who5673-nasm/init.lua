c60d341780e276700032bf034e292de616361bee
Who5673 <windows7modrenedition@gmail.com>
2025-07-11 14:51:46 +0700

    who5673-nasm commit

--- lua/who5673-nasm/init.lua
+++ lua/who5673-nasm/init.lua
@@ -7,7 +7,7 @@ function M.setup()
 	-- Load snippets like CSCode style from the local dir plugin
 	-- If you want to add more snippets in the future, you can add:
 	loader2.load({
-		path = vim.fn.stdpath("data") .. "/lazy/who5673-nasm/lua/who5673-snippets",
+		path = vim.fn.stdpath("data") .. "/lazy/who5673-nasm/lua/who5673_snippets",
 	})
 end
 
