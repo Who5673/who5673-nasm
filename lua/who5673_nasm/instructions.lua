3befec6f0a6214c669a06a2bccce175e674c716c
Who5673 <windows7modrenedition@gmail.com>
2025-07-12 06:17:57 +0700

    who5673-nasm commit

---
+++ lua/who5673_nasm/instructions.lua
@@ -0,0 +1,17 @@
+-- lua/who5673_nasm/instructions.lua
+
+return {
+	mov = "Move data from source to destination",
+	inc = "Increment operand by 1",
+	dec = "Decrement operand by 1",
+	add = "Add source to destination",
+	sub = "Subtract source from destination",
+	xor = "Bitwise XOR",
+	and_ = "Bitwise AND",
+	or_ = "Bitwise OR",
+	cmp = "Compare operands",
+	jmp = "Unconditional jump",
+	je = "Jump if equal",
+	jne = "Jump if not equal",
+	-- thêm bao nhiêu tùy ý
+}
--- plugin/who5673-nasm.lua
+++ plugin/who5673-nasm.lua
@@ -1,6 +1,9 @@
 local cmp = require("cmp")
 local reg_source = require("who5673_nasm.completion_registers")
+local inst_source = require("who5673_nasm.completion_instructions")
 
 cmp.register_source("nasm_registers", reg_source)
+cmp.register_source("nasm_instructions", inst_source)
+
 require("who5673_nasm").setup()
-vim.notify("Plugin who5673 has loaded!")
+vim.notify("Plugin who5673-nasm has loaded!")
