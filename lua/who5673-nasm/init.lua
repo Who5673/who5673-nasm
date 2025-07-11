48de0afac67a84550f7d577187041ac4b51a93fc
Who5673 <windows7modrenedition@gmail.com>
2025-07-11 15:06:22 +0700

    who5673-nasm commit

--- lua/who5673-nasm/init.lua
+++ lua/who5673-nasm/init.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.setup()
 	local ls = require("luasnip")
+	local loader = require("luasnip.loaders.from_lua")
+	loader.load({
+		paths = vim.fn.stdpath("data") .. "/lazy/who5673-nasm/lua/who5673_snippets",
+	})
 	--local loader = require("luasnip.loaders.from_vscode")
-	local loader2 = require("luasnip.loaders.from_lua")
 	-- Load snippets like CSCode style from the local dir plugin
 	-- If you want to add more snippets in the future, you can add:
-	loader2.load({
-		path = vim.fn.stdpath("data") .. "/lazy/who5673-nasm/lua/who5673_snippets",
-	})
 end
 
 return M
