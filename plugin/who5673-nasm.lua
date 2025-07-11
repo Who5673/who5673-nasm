fe28430a70ec0a0167faba5eef828574035bbc9d
Who5673 <windows7modrenedition@gmail.com>
2025-07-11 23:38:40 +0700

    who5673-nasm commit

--- plugin/who5673-nasm.lua
+++ plugin/who5673-nasm.lua
@@ -1,6 +1,5 @@
 require("who5673-nasm").setup()
 local cmp = require("cmp")
-local reg_source = require("who5673-nasm.completion_registers")
 
-cmp.register_source("nasm_registers", reg_source)
+cmp.register_source("nasm_registers", require("who5673-nasm.completion_registers"))
 vim.notify("Plugin who5673 has loaded!")
